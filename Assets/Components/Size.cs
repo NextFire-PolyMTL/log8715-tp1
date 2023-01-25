3bcceacde6a88ad7b1ec2b1ec2a12832834f4150
Pierre-Emmanuel Rebours <pe.rebours@etu.emse.fr>
2023-01-25 18:56:13 -0500

    add compo + sys

---
+++ Assets/Components/Size.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Size : IComponent
+{
+    float size;
+}
---
+++ Assets/Components/Tags.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct isStatic : IComponent{}
+public struct isProtected : IComponent{}
+public struct isColliding : IComponent{}
+public struct isClicked : IComponent{}
