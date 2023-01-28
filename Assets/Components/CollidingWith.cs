25034d49ea696f5af0a7970d38cc1a15c771ffa7
NextFire <git@yuru.moe>
2023-01-28 13:26:36 -0500

    fix filename 2/2

--- Assets/Components/CollidingWith.cs
+++ Assets/Components/CollidingWith.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct collidingWith : IComponent
+public struct CollidingWith : IComponent
 {
     int[] collidedShapes;
 }
