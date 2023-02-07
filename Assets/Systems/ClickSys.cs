8cfd95275ff1b4159223426e1dba244245ddfbb9
Pierre-Emmanuel Rebours <pe.rebours@etu.emse.fr>
2023-02-07 12:22:25 -0500

    correction to ClickSys.cs

--- Assets/Systems/ClickSys.cs
+++ Assets/Systems/ClickSys.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 public class ClickSys : ISystem
 {
     public string Name => "ClickSys";
@@ -9,7 +10,7 @@ public class ClickSys : ISystem
         {
             var radius = World.Instance.GetComponent<Size>(entity).Radius;
 
-            if (Mathf.abs(mousePosition.x - position.X) < radius && Mathf.abs(mousePosition.y - position.Y) < radius)
+            if (Mathf.Abs(mousePosition.x - position.X) < radius && Mathf.Abs(mousePosition.y - position.Y) < radius)
             {
                 World.Instance.SetComponent<IsClicked>(entity, new IsClicked());
             }
