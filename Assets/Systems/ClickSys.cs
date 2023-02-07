34ae223ab2509366d3ceeecda9dca5caed1010f1
Pierre-Emmanuel Rebours <pe.rebours@etu.emse.fr>
2023-02-07 17:25:44 -0500

    click.cs marche avec click mais parfois explose la sphère 2 fois d'affilé (car execution trop rapide et bouton activé trop longtemps)

--- Assets/Systems/ClickSys.cs
+++ Assets/Systems/ClickSys.cs
@@ -11,13 +11,14 @@ public class ClickSys : ISystem
         
         //Trouver mieux /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
         var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
+        var mouseClick=(Input.GetMouseButton(0) || Input.GetMouseButton(1) || Input.GetMouseButton(2));
         World.Instance.ForEach<Position>((entity, position) =>
         {
             
             var radius = World.Instance.GetComponent<Size>(entity).Value.Radius;
-
-            if (Mathf.Abs(mousePosition.x - position.Value.X)< radius/2 && Mathf.Abs(mousePosition.y - position.Value.Y)< radius/2)
+           
+            if (Mathf.Abs(mousePosition.x - position.Value.X)< radius/2 && Mathf.Abs(mousePosition.y - position.Value.Y)< radius/2
+            && mouseClick)
             {
                 World.Instance.SetComponent<IsClicked>(entity, new IsClicked());
             }
