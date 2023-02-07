78f1df4f6b2f5929f89eb2f839094d16a2e3d78b
Pierre-Emmanuel Rebours <pe.rebours@etu.emse.fr>
2023-02-07 14:49:23 -0500

    correction of a condition in PositionSys

--- Assets/Systems/PositionSys.cs
+++ Assets/Systems/PositionSys.cs
@@ -14,10 +14,12 @@ public class PositionSys : ISystem
 
         World.Instance.ForEach<IsColliding>((entity, isColliding) =>
         {
+            
             if (!isColliding.HasValue)
             {
                 return;
             }
+            
 
             var position = World.Instance.GetComponent<Position>(entity);
             var velocity = World.Instance.GetComponent<Velocity>(entity);
@@ -26,7 +28,7 @@ public class PositionSys : ISystem
             //Trouver mieux /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
             //(et en faire peut-être une cst....)
             var screenBoundary = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-            if (collidedShapes.HasValue)
+            if (isColliding.HasValue && !collidedShapes.HasValue)
             {
                 if (Mathf.Abs(position.Value.X) + radius >= screenBoundary.x)
                 {
@@ -42,6 +44,7 @@ public class PositionSys : ISystem
                     World.Instance.SetComponent<Velocity>(entity, new Velocity(newPosVit.velocity1[0], newPosVit.velocity1[1]));
 
                 }
+
                 if (Mathf.Abs(position.Value.Y) + radius >= screenBoundary.y)
                 {
                     var newPosVit = CollisionUtility.CalculateCollision(
@@ -55,6 +58,7 @@ public class PositionSys : ISystem
                     World.Instance.SetComponent<Position>(entity, new Position(newPosVit.position2[0], newPosVit.position2[1]));
                     World.Instance.SetComponent<Velocity>(entity, new Velocity(newPosVit.velocity2[0], newPosVit.velocity2[1]));
                 }
+
                 World.Instance.RemoveComponent<IsColliding>(entity);
             }
         });
