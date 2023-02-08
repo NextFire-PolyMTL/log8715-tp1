3caec0b7e333f0aa7bc62e9cd96cc2511d590591
Pierre-Emmanuel Rebours <pe.rebours@etu.emse.fr>
2023-02-07 23:19:40 -0500

    avancé dans la collision des sphères: pb rencontré dans l'architecture, ne pas supprimer code commanté)

--- Assets/Systems/CollisionSys.cs
+++ Assets/Systems/CollisionSys.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 public class CollisionSys : ISystem
 {
@@ -13,10 +14,38 @@ public class CollisionSys : ISystem
             //Trouver mieux /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
             //(et en faire peut-être une cst....)
             var screenBoundary = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-            if (Mathf.Abs(position.Value.X) + radius/2 >= screenBoundary.x || Mathf.Abs(position.Value.Y) + radius/2 >= screenBoundary.y)
+            if (Mathf.Abs(position.Value.X) + radius / 2 >= screenBoundary.x || Mathf.Abs(position.Value.Y) + radius / 2 >= screenBoundary.y)
             {
                 World.Instance.SetComponent<IsColliding>(entity, new IsColliding());
             }
+            //Code à discuter
+            /*
+            World.Instance.ForEach<Position>((entity2, position2) =>
+            {
+                var radius2 = World.Instance.GetComponent<Size>(entity2).Value.Radius;
+                if (entity2.Id != entity.Id)
+                {
+                    if (Mathf.Sqrt(Mathf.Pow((position.Value.X - position2.Value.X),2)
+                    + Mathf.Pow((position.Value.Y - position2.Value.Y),2)) <= (radius + radius2) / 2)
+                    {
+                        World.Instance.SetComponent<IsColliding>(entity, new IsColliding());
+                        var collidingWith = World.Instance.GetComponent<CollidingWith>(entity);
+                        if (!collidingWith.HasValue)
+                        {
+                            World.Instance.SetComponent<CollidingWith>(entity, new CollidingWith(new int[] {entity2.Id}));
+                        }
+                        else{
+                            //c moche à changer /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
+                            int[] collidedShapes = collidingWith.Value.CollidedShapes;
+                            int[] newCollidedShapes = new int[collidedShapes.Length+1];
+                            Array.Copy(collidedShapes, newCollidedShapes, collidedShapes.Length);
+                            newCollidedShapes[collidedShapes.Length]=entity2.Id;
+                            World.Instance.SetComponent<CollidingWith>(entity, new CollidingWith(newCollidedShapes));
+                        }
+                    }
+                }
+            });
+            */
 
         });
 
--- Assets/Systems/PositionSys.cs
+++ Assets/Systems/PositionSys.cs
@@ -6,7 +6,7 @@ public class PositionSys : ISystem
 
     public void UpdateSystem()
     {
-        
+
 
         World.Instance.ForEach<IsColliding>((entity, isColliding) =>
         {
@@ -19,15 +19,15 @@ public class PositionSys : ISystem
             var position = World.Instance.GetComponent<Position>(entity);
             var velocity = World.Instance.GetComponent<Velocity>(entity);
             var radius = World.Instance.GetComponent<Size>(entity).Value.Radius;
-            var collidedShapes = World.Instance.GetComponent<CollidingWith>(entity);
-            
-             //Trouver mieux /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
+            var collidingWith = World.Instance.GetComponent<CollidingWith>(entity);
+
+            //Trouver mieux /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
             //(et en faire peut-être une cst....)
             var screenBoundary = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-            
-            if (isColliding.HasValue && !collidedShapes.HasValue)
+
+            if (isColliding.HasValue && !collidingWith.HasValue)
             {
-                if (Mathf.Abs(position.Value.X) + radius/2 >= screenBoundary.x)
+                if (Mathf.Abs(position.Value.X) + radius / 2 >= screenBoundary.x)
                 {
                     var newPosVit = CollisionUtility.CalculateCollision(
                         new Vector2(position.Value.X, position.Value.Y),
@@ -42,7 +42,7 @@ public class PositionSys : ISystem
 
                 }
 
-                if (Mathf.Abs(position.Value.Y) + radius/2 >= screenBoundary.y)
+                if (Mathf.Abs(position.Value.Y) + radius / 2 >= screenBoundary.y)
                 {
                     var newPosVit = CollisionUtility.CalculateCollision(
                         new Vector2(position.Value.X, position.Value.Y),
@@ -58,7 +58,34 @@ public class PositionSys : ISystem
 
                 World.Instance.RemoveComponent<IsColliding>(entity);
             }
-            
+            //code ci-dessous à discuter
+            /*
+            if(isColliding.HasValue && collidingWith.HasValue){
+                Debug.Log("ça marche");
+                var collidedShapes=collidingWith.Value.CollidedShapes;
+                foreach (var collidedShape in collidedShapes){
+
+                    var position2 = World.Instance.GetComponent<Position>(collidedShape);
+                    var velocity2 = World.Instance.GetComponent<Velocity>(collidedShape);
+                    var radius2 = World.Instance.GetComponent<Size>(collidedShape).Value.Radius;
+                    var newPosVit = CollisionUtility.CalculateCollision(
+                        new Vector2(position.Value.X, position.Value.Y),
+                        new Vector2(velocity.Value.Vx, velocity.Value.Vy),
+                        radius,
+                        new Vector2(position2.Value.X, position2.Value.Y),
+                        new Vector2(velocity2.Value.Vx, velocity2.Value.Vy),
+                        radius2
+                    );
+                    World.Instance.SetComponent<Position>(entity, new Position(newPosVit.position1[0], newPosVit.position1[1]));
+                    World.Instance.SetComponent<Velocity>(entity, new Velocity(newPosVit.velocity1[0], newPosVit.velocity1[1]));
+                
+                }
+                World.Instance.RemoveComponent<IsColliding>(entity);
+                World.Instance.RemoveComponent<CollidingWith>(entity);
+                
+            }
+            */
+
         });
 
         World.Instance.ForEach<Velocity>((entity, velocity) =>
